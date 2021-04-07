72bf2734241f68887793e7067eb8d825321d69e3
sal-isal <faizal.abdul.tif420@polban.ac.id>
2021-04-07 21:59:05 +0700

    Tugas Listing 4.1 - 4.4

---
+++ Listing4.3/src/com/belajar/Main.java
@@ -0,0 +1,67 @@
+package com.belajar;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee[] staff = new Employee[3];
+
+        staff[0] = new Employee("Tom", 40000);
+        staff[1] = new Employee("Dick", 60000);
+        staff[2] = new Employee("Harry", 65000);
+
+        // print out information about all Employee objects
+        for (Employee e : staff) {
+             e.setId();
+             System.out.println("name=" + e.getName() + ",id=" + e.getId() + ",salary=" + e.getSalary());
+        }
+
+        int n = Employee.getNextId(); // calls static method
+        System.out.println("Next available id=" + n);
+    }
+}
+
+class Employee
+{
+    public static int nextId = 1;
+
+    private String name;
+    private double salary;
+    private int id;
+
+    public Employee(String n, double s)
+    {
+        name = n;
+        salary = s;
+        id = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public static int getNextId() {
+        return nextId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId() {
+        id = nextId;
+        nextId++;
+    }
+
+    public static void main(String[] args) // unit test
+    {
+        Employee e = new Employee("Harry", 50000);
+        System.out.println(e.getName() + " " + e.getSalary());
+    }
+
+}
